795c96ab56412db8a7654ea713fd8f2ca060eb22
hoge528 <70310899+hoge528@users.noreply.github.com>
2023-11-23 12:29:58 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+int f() {
+    printf("hoge\n");
+    return 1;
+}
+
 int main() {
     int a = 3;
     int b = 3;
     printf("a: %d, b: %d, a + b = %d\n", a, b, a + b);
+
     return 0;
 }
